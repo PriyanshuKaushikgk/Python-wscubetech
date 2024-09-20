755e54135dfcdc4d8ff6bdae11c9b00cb2630e1c
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-09-20 22:35:17 +0530

    newly

--- conditional.py
+++ conditional.py
@@ -1,7 +1,28 @@
-print('hi sir')
+# print('hi sir')
+
+# n = int(input("Enter number:"))
+# if (n%2==0):
+#     print("Prime number")
+# else:
+#     print('not prime')
+
+
 
 n = int(input("Enter number:"))
-if (n%2==0):
-    print("Prime number")
+if (n==1):
+    print('sunday')
+elif (n==2):
+    print('monday')
+elif (n==3):
+    print('tuesday')
+elif (n==4):
+    print('wednesday')
+elif (n==5):
+    print('thrusday')
+elif (n==6):
+    print('friday')
+elif (n==7):
+    print('saturday')
+
 else:
-    print('not prime')
+    print('input mismatch')
