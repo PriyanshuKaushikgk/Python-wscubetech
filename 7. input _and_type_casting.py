744e7dfec041235c8cb960f516e1c83dbe0f42f0
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-09-21 23:19:36 +0530

    update

---
+++ 7. input _and_type_casting.py
@@ -0,0 +1,31 @@
+# a = input("Enter the value1:-")
+# b = input("Enter the value2:-")
+# print(a+b)
+
+# TYPE CASTING  --->> 1. int
+# a = int(input("Enter the value1:-"))
+# b = int(input("Enter the value2:-"))
+# print(a+b)
+
+
+# 2. float
+# c= float(input("Enter the value1:-"))
+# d = float(input("Enter the value2:-"))
+# print(c+d)
+
+
+#  3. Eval
+
+c= eval(input("Enter the value1:-"))
+d = eval(input("Enter the value2:-"))
+print(c+d)
+
+
+
+
+
+
+
+
+
+
