b4749ab13a2c1b80e6d3c89637bb2aed38de513a
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-09-22 00:11:03 +0530

    conditional statements questions practise

--- Conditional_questions.py
+++ Conditional_questions.py
@@ -1,9 +1,22 @@
 # Q.1  23----89 
 # this number is between 23 to 89
 
-a  = int(input("enter the number:-"))
-if (a>=23 and a<=89):
-    print(a,"This number is between 23 to 89")
+# a  = int(input("enter the number:-"))
+# if (a>=23 and a<=89):
+#     print(a,"This number is between 23 to 89")
 
-else:
-    print('mis match')
+# else:
+#     print('mis match')
+
+
+# Q.2 this number is between 11 to 20
+
+# a  = int(input("enter the number:-"))
+# if (a>=11 and a<=20):
+#     print(a,":","this number is between 11 and 20")
+# else:
+#     print("mis match")
+
+# Q.3 user input this number 7 and 3 both valid not?
+
+	
