bb2f328a23ad1b95f0c9a95be89eb2eb4a6bf425
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-09-21 23:42:15 +0530

    Quetions practise for conditional statements

---
+++ Conditional_questions.py
@@ -0,0 +1,9 @@
+# Q.1  23----89 
+# this number is between 23 to 89
+
+a  = int(input("enter the number:-"))
+if (a>=23 and a<=89):
+    print(a,"This number is between 23 to 89")
+
+else:
+    print('mis match')
