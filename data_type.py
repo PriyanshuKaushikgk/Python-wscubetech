eb447492818b6b0ea65fcc0f13b4615520417dec
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-09-21 23:04:02 +0530

    datatypes

---
+++ data_type.py
@@ -0,0 +1,80 @@
+# In python there are 2 data types :
+    # 1. Mutable Data Type : Mutable DAta Type are those datatype that can be change any value .
+    # 2. Immutable Data Type : Are those datatpye that can not be change any value  .
+
+    # IMMutable data type :
+        # 1.  integers
+        # 2.  Float
+        # 3.   Complex Number
+        # 4. String 
+        # 5. Tuple
+        # 6. List
+
+    #  Mutable Data type:
+        # 1. Dictionary
+        # 2. Set
+        # 3. Byte Array
+
+
+# a= 10
+# v = 20.1
+# d = 2j+3
+# print(a,type(a))
+# print(v,type(v))
+# print(d,type(d))
+
+
+# STRING: -->>> A String is a collection of one or more characters put in a single quote, double_quote or Triple quote .
+#  Multi_line Strings can be denoted using triple quotes, ''' or """
+
+# s = "This  is a boy"
+# print(s ,type(s))
+
+# D =  """hello789542"""
+# print(D,type(D))
+
+# e = '10'
+# print(e,type(e))
+
+
+# LIST:---->>>>>  List is an ordered sequence of items.
+#  It is one of the most used datatype in python and is very flexible.
+# []
+
+# l = [10,5.5,'kaushik']
+# l[2]=30
+# print(l,type(l))
+
+# TUPLE: ------>>>>>>>>>>> Tupel is an ordered of items same as a list.
+#  if is defined within paraentheses() where items are separated by commas.
+
+# t = (5,'hello',1+3j)
+
+# print(t,type(t))
+
+# DICTIONARY:----------->>>>>> Dictionary is an unordered collection of key-value pairs.
+
+# In python , dictionary are defined within braces{} with each item being a pair in the form key:value
+
+# d = {
+#     'course_name':'Python',
+#     'Course-duration':'2 Months'
+
+# }
+# print(d['Course-duration'])
+# print(d,type(d))
+
+
+
+# SET :------>>>>> A set is an unordered collection of items .
+#  Every set elements is unique (no duplicates) and must be immutable (Cannot be changed)
+# {}
+
+
+# my_set = {1,2,3,4,5}
+# print(my_set,type(my_set))
+
+
+# s= {1,2,1,5,3,4,2,5}
+# print(s) 
+
