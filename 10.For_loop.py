6f585907c30693d40bbe2b90190cfb547643146c
priyanshu kaushik <priyanshukaushikdk@gmail.com>
2024-09-23 20:33:23 +0530

    update

---
+++ 10.For_loop.py
@@ -0,0 +1,18 @@
+# range m condition aati hai :---------->>>>>>>>{range(start, stop(condition), increment)}
+
+for i in range(1,11):
+    print("2 x",i,"=",2*i)
+
+
+# Reverse case in for loop------->>>>>
+
+# for i in range(10,0,-1):
+#     print(i)
+# print()
+# for i in range(10,-1,-2):
+#     print(i)
+
+# print()
+
+# for i in range(10,-1,-1):
+#     print("2x",i,"=",2*i)
