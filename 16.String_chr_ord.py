5dd07c1c7f1d6193af719ee604200f74952809dd
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-09-28 21:52:40 +0530

    string chr or ord function

---
+++ 16.String_chr_ord.py
@@ -0,0 +1,24 @@
+# This takes in an interger i and converts it to a character c , so it returns a character string.
+# Convert integer 65 to ASCII character('A')
+
+# y = chr(65)
+# print(type(y),y)
+
+# a = 65
+# print(chr(a))
+
+
+# The ORD() Function
+
+# This takes a single unicode character(string of length 1) and returns an integer, so the format is:
+
+# Convert ASCII Unicode Character 'A' to 65
+
+y = ord('a')
+print(type(y),y)  
+
+t = 'A'
+print(ord(t))
+
+a ="h"
+print(ord(a))
