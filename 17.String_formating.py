b3af38dcb9636f64347f3d4020bc4c507d3c4334
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-09-28 21:53:18 +0530

    String formating

---
+++ 17.String_formating.py
@@ -0,0 +1,19 @@
+# String formating Method:--->>>>>  The format() method formats the specified value(s) and insert the string's placeholder.
+# The placeholder is defined using curly brackets:{} . Read more about the placeholders in ht placeholder section below.
+
+
+w = "Welcome {} to {} uscubetech".format("hello",20)
+print(w)
+
+t = "Welcome {0} to {1} wscubetech".format("hello",20)
+print(t)
+
+r="Welcome {a} to {b}  wscubetech".format(a= 30,b=20)
+print(r)
+r="Welcome {a:11} to {b}  wscubetech".format(a= 30,b=20)
+print(r)
+r="Welcome {a:^10} to {b}  wscubetech".format(a= 30,b=20)
+print(r)
+r="Welcome {a:<10} to {b:^10}  wscubetech".format(a= 30,b=20)
+print(r)
+
