70e99681ece34f87e6c806427d02cf193afbac35
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-09-29 21:02:05 +0530

    list function and comprehension

---
+++ 20.list_comprehension.py
@@ -0,0 +1,23 @@
+#  list compehension is an elegant way to define and create lists based on existing lists.
+# list comprehension is generally more compact and faster than normal functions and loops for creating list.
+
+# syntax of list comprehension
+# [expression for item in list]
+
+
+l = []
+for a in range(1,101):
+    l.append(a)
+print(l)
+
+print()
+
+n = [m for m in range(1,101)]
+print(n)  # USE same vairable like m in m.
+
+n = [m for m in range(1,101) if m%2==0]
+print(n)
+
+s = 'hello'
+d = [g for g in s]
+print(d)
---
+++ 21.list_function.py
@@ -0,0 +1,51 @@
+# fucntion for Delete Element from list-
+# del
+# pop()
+# remove()
+# clear()
+
+# l = [10,20,30,40,50]
+# del l[1]
+# print(l)
+
+# l = [10,20,30,40,50]
+
+# print(l.pop(0))  #ye pop ne lo pop kiya h usko btata hai
+# print(l)
+
+# l = [10,20,30,40,50]
+# l.remove(30)    #value ko remove krta h
+# print(l)
+
+# l = [10,20,30,40,50]
+# l.clear()  # complete list ko blank kr dega.
+# print(l)
+
+
+#  ------  update function------
+# update Elements from list:
+# l = [20,30,40,50]
+# l[0]=90
+# print(l)
+
+# #   ----------update Elements from list:------
+# insert()
+# append()
+# extends()
+
+# l = [10,20,30,40,50]
+# l.insert(0,70)
+# print(l)
+
+# l = [10,20,30,40,50]
+# n=[60,70]
+# l.append(n)
+# # l.append(80)
+# print(l)
+
+
+l = [10,20,30,40,50]
+n=[60,70]
+l.extend(n)
+# l.append(80)
+print(l)
