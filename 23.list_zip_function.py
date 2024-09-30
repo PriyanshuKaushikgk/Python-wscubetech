628fd51cbc166b68f7cee0188a20b0b3e17ad3f9
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-09-30 22:15:14 +0530

    List zip function

---
+++ 23.list_zip_function.py
@@ -0,0 +1,12 @@
+# list iterate over 2+ list at the same time(zip function)
+
+# l = [10,20,30,40]
+# l1= [3,5,7,88]
+# for a,b in zip(l,l1):
+#     print(a,b)
+    
+# #  Another method:----
+
+# t = len(l)
+# for h in range(t):
+#     print(l[h],l1[h])
