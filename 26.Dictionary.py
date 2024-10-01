560dd38c076599402728a14c966acec587d4e178
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-10-01 21:15:12 +0530

    Dictionary

---
+++ 26.Dictionary.py
@@ -0,0 +1,19 @@
+# Dictionary{}:--->>>>>> 
+# A dictionary is a colllection which is unordered. In python Dictionaries are written with curly brackets{}
+# it is unordered Data type.
+# it works on key and value pair
+# {}
+# It is mutable data type
+
+d = {
+    'name': 'python',
+    'fees': 8000,
+    'duration': "2 months"
+
+}
+# print(type(d))
+# print(d['name'])
+# print(d['duration'])
+for n in d:
+    # print(n)
+    print(n,":",d[n])
