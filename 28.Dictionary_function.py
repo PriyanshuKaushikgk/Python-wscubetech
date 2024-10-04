d50a4c89db48b346cae3dadb175431d76d3aee79
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-10-04 20:21:35 +0530

    dictionary function

---
+++ 28.Dictionary_function.py
@@ -0,0 +1,36 @@
+# Dictionary function:------>  get, keys,values,items,del,pop,dict,update,clear
+# d = {
+#     'course':'python',
+#     'fees':'8000',
+#     'duration':'2 months'
+
+# }
+# c=d.get('course')
+# print(c)
+
+# for a in d.keys():
+#     print(a)
+
+# for a in d.values():
+#     print(a)
+
+# for a,b in d.items():
+# #     print(a,":",b)
+
+# del d['fees']
+# print(d)
+
+# d.pop('duration')
+# print(d)
+
+# a = dict(name = 'mern', fees = 5200)
+# print(a)
+
+# d.update({'fees':100000})
+# print(d)
+
+# d['desc']="this is python "
+# print(d)
+
+# d['fees']=212548
+# print(d)
